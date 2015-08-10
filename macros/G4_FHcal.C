408d1b5996da911807e6f05d0b186503f629fae4
Nils Feege <nils.feege@stonybrook.edu>
2015-08-09 23:54:12 -0400

    Automatically generate path to mapping file for ForwardHcal

--- macros/G4_FHcal.C
+++ macros/G4_FHcal.C
@@ -7,6 +7,17 @@ void FHCalInit()
 double G4_FHCal(PHG4Reco* g4Reco, const int absorberactive = 0, const char * name = "FHCAL")
 {
   PHG4ForwardHcalSubsystem *hhcal = new PHG4ForwardHcalSubsystem(name);
+
+  // automatically generate path to mapping file
+  ostringstream mapping_hhcal;
+  mapping_hhcal << getenv("OFFLINE_MAIN") <<
+    "/share/calibrations/ForwardHcal/mapping/towerMap_hHcal_v0.txt";
+  cout << mapping_hhcal.str() << endl;
+
+  // configure detector
+  hhcal->SetTowerMappingFile( mapping_hhcal.str() );
   hhcal->OverlapCheck(overlapcheck);
+
+  // register detector
   g4Reco->registerSubsystem( hhcal );
 }
