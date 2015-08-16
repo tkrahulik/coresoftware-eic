302cdc90316c14fea3912c25e9337382ee5ee776
Nils Feege <nils.feege@stonybrook.edu>
2015-08-16 12:03:12 -0400

    Update version of used mapping files

--- macros/G4_EEmc.C
+++ macros/G4_EEmc.C
@@ -8,9 +8,9 @@ double G4_EEmc(PHG4Reco* g4Reco)
 
   /* Use non-projective geometry */
   ostringstream mapping_eecal;
-  //mapping_eecal << "calibrations/CrystalCalorimeter/mapping/towerMap_EEMC_v001.txt";
+  //mapping_eecal << "calibrations/CrystalCalorimeter/mapping/towerMap_EEMC_v002.txt";
   mapping_eecal << getenv("OFFLINE_MAIN") <<
-    "/share/calibrations/CrystalCalorimeter/mapping/towerMap_EEMC_v001.txt";
+    "/share/calibrations/CrystalCalorimeter/mapping/towerMap_EEMC_v002.txt";
   cout << mapping_eecal.str() << endl;
   eecal->SetTowerMappingFile( mapping_eecal.str() );
 
--- macros/G4_FEmc.C
+++ macros/G4_FEmc.C
@@ -9,12 +9,12 @@ double G4_FEmc(PHG4Reco* g4Reco, const int absorberactive = 0, const char * name
   PHG4ForwardEcalSubsystem *hemc = new PHG4ForwardEcalSubsystem(name);
 
   ostringstream mapping_hemc;
-  //  mapping_hemc << "calibrations/ForwardEcal/mapping/towerMap_FEMC_v001.txt";
+  //mapping_hemc << "calibrations/ForwardEcal/mapping/towerMap_FEMC_v002.txt";
 
-  // automatically generate path to mapping file
-  mapping_hemc << getenv("OFFLINE_MAIN") <<
-    "/share/calibrations/ForwardEcal/mapping/towerMap_FEMC_v001.txt";
-  cout << mapping_hemc.str() << endl;
+    // automatically generate path to mapping file
+    mapping_hemc << getenv("OFFLINE_MAIN") <<
+      "/share/calibrations/ForwardEcal/mapping/towerMap_FEMC_v002.txt";
+    cout << mapping_hemc.str() << endl;
 
   // configure detector
   hemc->SetTowerMappingFile( mapping_hemc.str() );
